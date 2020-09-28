2ba06f529c0b190446be8ada629362520191a29b
definayt <definayt@users.noreply.github.com>
2020-09-28 13:50:34 +0700

    Update rinba_official.sql

--- rinba_official.sql
+++ rinba_official.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2020 at 07:08 PM
+-- Generation Time: Sep 28, 2020 at 08:49 AM
 -- Server version: 10.1.39-MariaDB
 -- PHP Version: 7.3.5
 
@@ -39,7 +39,7 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id_contact`, `jenis_contact`, `contact`) VALUES
-(1, 'Whatsapp', '081229476730'),
+(1, 'Whatsapp', '6281229476730'),
 (2, 'Instagram', 'instagram.com/rinba.official');
 
 -- --------------------------------------------------------
@@ -101,6 +101,25 @@ CREATE TABLE `produk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `id_kategori`, `nama_produk`, `deskripsi_produk`, `gambar_produk_1`, `gambar_produk_2`, `harga`, `stok`) VALUES
+(1, 1, 'Contoh Produk 1', 'A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.\r\n\r\nOn her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word \"and\" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.', 'Rinba_Official.png', 'no_image.jpg', 300000000, 0),
+(2, 2, 'Contoh Produk 2', '-', 'no_image.jpg', 'no_image.jpg', 3000, 2),
+(3, 2, 'Sample Produk 3', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2),
+(4, 3, 'Contoh Produk 4', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2),
+(5, 3, 'Sample Produk 5', '-', 'no_image.jpg', 'no_image.jpg', 6000, 2),
+(6, 3, 'Sample Produk 6', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 0),
+(7, 3, 'Sample Produk 7', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2),
+(8, 3, 'Sample Produk 8', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2),
+(9, 3, 'Sample Produk 9', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2),
+(10, 3, 'Sample Produk 10', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2),
+(11, 3, 'Sample Produk 11', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2),
+(12, 3, 'Sample Produk 12', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 0),
+(29, 3, 'Sample Produk 13', '-', 'no_image.jpg', 'no_image.jpg', 300000000, 2);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -143,19 +162,19 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
